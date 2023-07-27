7146bcd6754def9859c9bef444adc0049d81e967
OmNikam04 <80584774+OmNikam04@users.noreply.github.com>
2023-07-27 10:21:06 +0530

    address book complete

--- OSL/01/pr01.sh
+++ OSL/01/pr01.sh
@@ -1,57 +1,55 @@
 #!/bin/bash
 
 #Creating file to store the database
-echo "File name:"
-read fileName
-touch $fileName
+create() {
+    echo "File name:"
+    read fileName
+    fileName=$fileName
+    touch $fileName
+}
 
 add() {
     echo "Enter number of records to insert: "
     read records
 
-    for (( i=0 ; i < $records ; i++ ))
-    do
+    for ((i = 0; i < $records; i++)); do
 
-    echo "Enter id:"
-    read id
+        echo "Enter id:"
+        read id
 
-    echo "Enter name:"
-    read name
+        echo "Enter name:"
+        read name
 
-    echo "Enter marks:"
-    read marks
+        echo "Enter marks:"
+        read marks
 
-    echo -e "$id\t$name\t$marks" >> $fileName
+        echo -e "$id\t$name\t$marks" >>$fileName
     done
 }
 
-show () {
+show() {
     cat $fileName
 }
 
-
-search () {
+search() {
     read -p "Enter id to search: " to_search
 
-    if grep -wi $to_search $fileName
-    then
+    if grep -wi $to_search $fileName; then
         echo "Found the record"
     else
         echo "Not found the record"
     fi
 }
 
-
-delete () {
+delete() {
     read -p "Enter id to delete: " to_search
 
-    if grep -wi $to_search $fileName
-    then
+    if grep -wi $to_search $fileName; then
         # Logic to delete file
         # find the all all the lines that do not matches the pattern using -v flag of grep command
         # print all those lines into sepearate file temp
         echo "Record being deleted: "
-        grep -wv $to_search $fileName >> temp
+        grep -wv $to_search $fileName >>temp
         # delete the original file
         rm $fileName
         # rename the temp as our old file name
@@ -61,5 +59,63 @@ delete () {
     fi
 }
 
+modify() {
+    read -p "Enter id to delete: " to_search
 
-delete 
+    if grep -wi $to_search $fileName; then
+        grep -wv $to_search $fileName >>temp
+        rm $fileName
+        mv temp $fileName
+
+        echo "Enter id:"
+        read id
+
+        echo "Enter name:"
+        read name
+
+        echo "Enter marks:"
+        read marks
+
+        echo -e "$id\t$name\t$marks" >>$fileName
+
+    else
+        echo "Record does not exist"
+    fi
+}
+
+choice=0
+
+while [ $choice -lt 7 ]; do
+    echo -e "\nEnter your choice: "
+    echo -e "1:Create address book "
+    echo -e "2:View Address book "
+    echo -e "3:Insert Record "
+    echo -e "4:Delete Record "
+    echo -e "5:Search Record "
+    echo -e "6:Modify Record "
+    echo -e "7:Exit"
+    read choice
+    case $choice in
+    1)
+        create
+        ;;
+    2)
+        show
+        ;;
+    3)
+        add
+        ;;
+    4)
+        delete
+        ;;
+    5)
+        search
+        ;;
+    6)
+        modify
+    ;;
+    *)
+        echo "Thanks for using our services..."
+        ;;
+    esac
+done
