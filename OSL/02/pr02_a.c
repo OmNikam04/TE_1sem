5550e68bf09fc9ee775ec67082b9494172126465
OmNikam04 <80584774+OmNikam04@users.noreply.github.com>
2023-11-07 08:31:28 +0530

    Update pr02_a.c

--- OSL/02/pr02_a.c
+++ OSL/02/pr02_a.c
@@ -1,26 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <sys/types.h>
 #include <sys/wait.h>
+#include <sys/types.h>
 
-void merge(int arr[], int left, int mid, int right) {
-    int n1 = mid - left + 1;
-    int n2 = right - mid;
+void bubblesort(int arr[],int n){
+	for(int i=0;i<n-1;i++){
+		for(int j=0;j<n-i-1;j++){
+			if(arr[j]>arr[j+1]){
+				int temp=arr[j];
+				arr[j]=arr[j+1];
+				arr[j+1]=temp;
+			}
+		}
+	}
+}
+
+void merge(int arr[], int l, int m, int r) {
+    int n1 = m - l + 1;
+    int n2 = r - m;
 
     int L[n1], R[n2];
 
-    for (int i = 0; i < n1; i++) {
-        L[i] = arr[left + i];
-    }
-    for (int j = 0; j < n2; j++) {
-        R[j] = arr[mid + 1 + j];
-    }
+    for (int i = 0; i < n1; i++)
+        L[i] = arr[l + i];
+    for (int j = 0; j < n2; j++)
+        R[j] = arr[m + 1 + j];
 
-    int i = 0;
-    int j = 0;
-    int k = left;
-
+    int i = 0, j = 0, k = l;
     while (i < n1 && j < n2) {
         if (L[i] <= R[j]) {
             arr[k] = L[i];
@@ -45,93 +52,124 @@ void merge(int arr[], int left, int mid, int right) {
     }
 }
 
-void mergeSort(int arr[], int left, int right) {
-    if (left < right) {
-        int mid = left + (right - left) / 2;
-
-        mergeSort(arr, left, mid);
-        mergeSort(arr, mid + 1, right);
-
-        merge(arr, left, mid, right);
+void mergesort(int arr[], int l, int r) {
+    if (l < r) {
+        int m = l + (r - l) / 2;
+        mergesort(arr, l, m);
+        mergesort(arr, m + 1, r);
+        merge(arr, l, m, r);
     }
 }
 
-int partition(int arr[], int low, int high) {
-    int pivot = arr[high];
-    int i = (low - 1);
-
-    for (int j = low; j <= high - 1; j++) {
-        if (arr[j] < pivot) {
-            i++;
-            int temp = arr[i];
-            arr[i] = arr[j];
-            arr[j] = temp;
-        }
-    }
-    int temp = arr[i + 1];
-    arr[i + 1] = arr[high];
-    arr[high] = temp;
-    return (i + 1);
-}
-
-void quickSort(int arr[], int low, int high) {
-    if (low < high) {
-        int pi = partition(arr, low, high);
-
-        quickSort(arr, low, pi - 1);
-        quickSort(arr, pi + 1, high);
-    }
-}
-
-int main() {
-    int n;
-    printf("Enter the number of integers: ");
-    scanf("%d", &n);
-
-    int arr[n];
-    printf("Enter %d integers: ", n);
-    for (int i = 0; i < n; i++) {
-        scanf("%d", &arr[i]);
-    }
-
-    pid_t pid = fork(); // Create a child process
-
-    if (pid < 0) {
-        perror("Fork failed");
-        exit(1);
-    } else if (pid == 0) {
-        // Child process
-        printf("\nChild process (ID: %d) is sorting using Quick Sort:\n", getpid());
-        quickSort(arr, 0, n - 1);
-        printf("Sorted by Child Process: ");
-        for (int i = 0; i < n; i++) {
-            printf("%d ", arr[i]);
-        }
-        printf("\n");
-    } else {
-        // Parent process
-        printf("\nParent process (ID: %d) is sorting using Merge Sort:\n", getpid());
-        mergeSort(arr, 0, n - 1);
-        printf("Sorted by Parent Process: ");
-        for (int i = 0; i < n; i++) {
-            printf("%d ", arr[i]);
-        }
-        printf("\n");
-
-        // Wait for the child process to finish
-        int status;
-        wait(&status);
-
-        // Demonstrate the zombie state by waiting for user input
-        printf("\nParent process (ID: %d) is waiting...\n", getpid());
-        getchar(); // Wait for user input to proceed
-
-        // Demonstrate the orphan state by making the parent sleep
-        printf("\nParent process (ID: %d) is sleeping for 5 seconds...\n", getpid());
-        sleep(5); // Sleep for 5 seconds
-
-        printf("\nParent process (ID: %d) is exiting.\n", getpid());
-    }
-
-    return 0;
+int main(){
+	printf("PARENT: MY PID IS %d\n",getpid());
+	int n;
+	printf("Enter the size of the array: ");
+	scanf("%d",&n);
+	int arr[n];
+	printf("Enter the elements of the array:- ");
+	for(int i=0;i<n;i++){
+		scanf("%d",&arr[i]);
+	}
+	int ch;
+	printf("******************************************************\n");
+	printf("***********************MENU*************************\n");
+	printf("!.Fork system call (Merge Sort/Bubble Sort)\n");
+	printf("2.Create Orphan State.\n");
+	printf("3.Create Zombie State.\n");
+	printf("******************************************************\n");
+	printf("Enter you choice:- ");
+	scanf("%d",&ch);
+	printf("FORKING..\n");
+	int p;	
+	p=fork();
+	switch(ch){
+		case 1:{
+			if(p<0){
+				printf("FORK FAILED!\n");
+			}
+			else if(p==0){
+				printf("CHILD: MY PID IS %d\n",getpid());
+				printf("CHILD: MY PARENTS PID IS %d\n",getppid());
+				bubblesort(arr,n);
+				printf("Sorted array by bubble sort:- ");
+				for(int i=0;i<n;i++){
+					printf("%d",arr[i]);
+				}
+				printf("\n");
+				printf("CHILD: I am dying now.\n");
+				printf("_________________________________________________________________\n");
+			}
+			else{
+				wait(NULL);
+				printf("PARENT: I am back.\n");
+				printf("PARENT: MY PID IS %d\n",getpid());
+				printf("PARENT: MY CHILDS PID IS %d\n",p);
+				mergesort(arr,0,n-1);
+				printf("Sorted array by merge sort:- ");
+				for(int i=0;i<n;i++){
+					printf("%d",arr[i]);
+				}
+				printf("\n");
+				printf("PARENT: I am dying now.\n");
+				printf("_________________________________________________________________\n");
+			}
+			break;
+		}
+		case 2:{
+			if(p<0){
+				printf("FORK FAILED!\n");
+			}
+			else if(p==0){
+				printf("CHILD: MY PID IS %d\n",getpid());
+				printf("CHILD: MY PARENTS PID IS %d\n",getppid());
+				printf("CHILD: I am sleeping now.\n");
+				sleep(10);
+				printf("_________________________________________________________________\n");
+				system("ps -elf | grep a.out");
+			}
+			else{
+				printf("PARENT: I am back.\n");
+				printf("PARENT: MY PID IS %d\n",getpid());
+				printf("PARENT: MY CHILDS PID IS %d\n",p);
+				printf("PARENT: I am dying now.\n");
+				printf("_________________________________________________________________\n");
+				system("ps -elf | grep a.out");
+				exit(0);
+			}
+			break;
+		}
+		case 3:{
+			if(p<0){
+				printf("FORK FAILED!\n");
+			}
+			else if(p==0){
+				sleep(2);
+				printf("CHILD: MY PID IS %d\n",getpid());
+				printf("CHILD: MY PARENTS PID IS %d\n",getppid());
+				printf("CHILD: I am dying now.\n");
+				printf("_________________________________________________________________\n");
+                // e for showing all process
+                // l for long list format
+                // f show process hiearchy in tree like strucuter
+				system("ps -elf | grep a.out");
+				exit(0);
+			}
+			else{
+				printf("PARENT: I am back.\n");
+				printf("PARENT: MY PID IS %d\n",getpid());
+				printf("PARENT: MY CHILDS PID IS %d\n",p);
+				printf("PARENT: I am sleeping now.\n");
+				sleep(10);
+				printf("_________________________________________________________________\n");
+				system("ps -elf | grep a.out");
+			}
+			break;
+		}
+		case 4: {
+			break;
+		}
+	}
+	
+	return 0;
 }
